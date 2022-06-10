f263a6258c67708f79db6e51ab58d9e399a03984
Oscar Sönnerborg <sonner_borg@hotmail.com>
2022-06-10 16:01:42 +0200

    Added instance of person

--- Person.cs
+++ Person.cs
@@ -8,6 +8,15 @@ namespace BranschesÖvning
 {
     internal class Person
     {
+        private string v1;
+        private int v2;
+
+        public Person(string v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
         public string Name { get; set; }
         public int Age { get; set; }
     }
