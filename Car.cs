2f54acbf6876df7acf2d97944670c5df65f385d0
Yousif Ali <yousifali95@hotmail.co.uk>
2022-06-10 15:59:38 +0200

    Added some cool stuff

--- Car.cs
+++ Car.cs
@@ -2,6 +2,12 @@ namespace BranschesÖvning
 {
     internal class Car
     {
+        public Car(string make, string model)
+        {
+            Make = make;
+            Model = model;
+        }
+
         public string Make { get; set; }
         public string Model { get; set; }
     }
