44476d82f1a585d24ef4e6984b51d7a18c7c5885
Oscar Sönnerborg <sonner_borg@hotmail.com>
2022-06-10 15:55:28 +0200

    Added Class Person

---
+++ Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranschesÖvning
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
